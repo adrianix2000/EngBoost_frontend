3a0c3492152402e20d9f9b812314bcfd34d79328
Adrian Sak <adrianvsak@gmail.com>
2023-12-29 16:17:47 +0100

    add registration form with inputs

--- src/app/modules/authorization/authorization.module.ts
+++ src/app/modules/authorization/authorization.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthorizationRoutingModule } from './authorization-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [CommonModule, AuthorizationRoutingModule, SharedModule],
 })
 export class AuthorizationModule {}
