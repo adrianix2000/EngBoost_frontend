6e3dd69ab630e4934846b6ef4f0804947c8d9740
Adrian Sak <adrianvsak@gmail.com>
2024-01-11 00:22:33 +0100

    adding logout button in navbar

--- src/app/modules/core/components/navbar/navbar.component.ts
+++ src/app/modules/core/components/navbar/navbar.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../../services/token.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  constructor(public tokenService: TokenService) {}
+
+  showLogoutButton!: boolean;
+
+  logout(): void {
+    this.tokenService.destroyToken();
+    console.log('logout');
+  }
+
+  ngOnInit(): void {
+    this.tokenService.mySubject.subscribe({
+      next: (value) => {
+        this.showLogoutButton = value;
+        console.log(this.showLogoutButton);
+      },
+    });
+  }
+}
--- src/app/modules/core/services/token.service.ts
+++ src/app/modules/core/services/token.service.ts
@@ -1,17 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenService {
+  public mySubject = new Subject<boolean>();
   constructor() {}
 
   setToken(token: string): void {
     localStorage.setItem('token', token);
+    this.mySubject.next(this.isTokenExists());
   }
 
   getToken(): string | null {
     let token = localStorage.getItem('token');
     return token;
+  }
+
+  isTokenExists(): boolean {
+    const token = localStorage.getItem('token');
+    return token != null;
+  }
+
+  destroyToken(): void {
+    localStorage.removeItem('token');
+
+    console.log('wywoluje sie');
+
+    this.mySubject.next(this.isTokenExists());
   }
 }
